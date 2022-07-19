c40df9cdf3ea677ffa5aa85366a3783770d8f445
Emmanuel Peter <Peterojonilemi@gmail.com>
2022-07-19 17:15:07 +0100

    Added the message to be sent to the web brower

--- sock.c
+++ sock.c
@@ -75,6 +75,33 @@ int main()
     char address_buffer[100];
     getnameinfo((struct sockaddr *)&client_address, client_len, address_buffer, sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
     printf("%s\n", address_buffer);
+
+    // Client side programming
+    printf("Reading request...\n");
+    char request[1024];
+    int bytes_received = recv(socket_client, request, 1024, 0);
+    printf("Received %d bytes.\n", bytes_received);
+    printf("%.*s", bytes_received, request);
+
+    // sending our response
+    printf("Sending response...\n");
+    const char *response = "HTTP/1.1 200 OK\r\n"
+                           "Connection: close\r\n"
+                           "Content-Type: text/plain\r\n\r\n"
+                           "Local time is: ";
+    int bytes_sent = send(socket_client, response, strlen(response), 0);
+    printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(response));
+
+    // the time program
+    time_t timer;
+    time(&timer);
+    char *time_msg = ctime(&timer);
+    bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
+    printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(time_msg));
+
+    printf("Closing connection...\n");
+    CLOSESOCKET(socket_client);
+
 #if defined(_WIN32)
     WSACleanup();
 #endif
