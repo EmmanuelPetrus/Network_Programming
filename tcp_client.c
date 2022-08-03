2df01f3e096a1476aa13cb27ac65c3a193c95818
Emmanuel Peter <Peterojonilemi@gmail.com>
2022-08-03 15:53:10 +0100

    added the right client process

--- tcp_client.c
+++ tcp_client.c
@@ -1,133 +1,140 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Lewis Van Winkle
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "chap03.h"
+
 #if defined(_WIN32)
-#include <conio.h> // this is included so that  _kbhit() function can be used in our program
+#include <conio.h>
 #endif
 
-int main()
-{
+int main(int argc, char *argv[]) {
 
 #if defined(_WIN32)
     WSADATA d;
-    if (WSAStartup(MAKEWORD(2, 2), &d))
-    {
+    if (WSAStartup(MAKEWORD(2, 2), &d)) {
         fprintf(stderr, "Failed to initialize.\n");
         return 1;
     }
 #endif
 
-    printf("Configuring local address...\n");
+    if (argc < 3) {
+        fprintf(stderr, "usage: tcp_client hostname port\n");
+        return 1;
+    }
+
+    printf("Configuring remote address...\n");
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+    struct addrinfo *peer_address;
+    if (getaddrinfo(argv[1], argv[2], &hints, &peer_address)) {
+        fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
 
-    struct addrinfo *bind_address;
-    getaddrinfo(0, "8080", &hints, &bind_address);
+
+    printf("Remote address is: ");
+    char address_buffer[100];
+    char service_buffer[100];
+    getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen,
+            address_buffer, sizeof(address_buffer),
+            service_buffer, sizeof(service_buffer),
+            NI_NUMERICHOST);
+    printf("%s %s\n", address_buffer, service_buffer);
+
 
     printf("Creating socket...\n");
-    SOCKET socket_listen;
-    socket_listen = socket(bind_address->ai_family,
-                           bind_address->ai_socktype, bind_address->ai_protocol);
-    if (!ISVALIDSOCKET(socket_listen))
-    {
+    SOCKET socket_peer;
+    socket_peer = socket(peer_address->ai_family,
+            peer_address->ai_socktype, peer_address->ai_protocol);
+    if (!ISVALIDSOCKET(socket_peer)) {
         fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
 
-    printf("Binding socket to local address...\n");
-    if (bind(socket_listen,
-             bind_address->ai_addr, bind_address->ai_addrlen))
-    {
-        fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
+
+    printf("Connecting...\n");
+    if (connect(socket_peer,
+                peer_address->ai_addr, peer_address->ai_addrlen)) {
+        fprintf(stderr, "connect() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
-    freeaddrinfo(bind_address);
+    freeaddrinfo(peer_address);
 
-    printf("Listening...\n");
-    if (listen(socket_listen, 10) < 0)
-    {
-        fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
-        return 1;
-    }
+    printf("Connected.\n");
+    printf("To send data, enter text followed by enter.\n");
 
-    fd_set master;
-    FD_ZERO(&master);
-    FD_SET(socket_listen, &master);
-    SOCKET max_socket = socket_listen;
+    while(1) {
 
-    printf("Waiting for connections...\n");
-
-    while (1)
-    {
         fd_set reads;
-        reads = master;
-        if (select(max_socket + 1, &reads, 0, 0, 0) < 0)
-        {
+        FD_ZERO(&reads);
+        FD_SET(socket_peer, &reads);
+#if !defined(_WIN32)
+        FD_SET(0, &reads);
+#endif
+
+        struct timeval timeout;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 100000;
+
+        if (select(socket_peer+1, &reads, 0, 0, &timeout) < 0) {
             fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
             return 1;
         }
 
-        SOCKET i;
-        for (i = 1; i <= max_socket; ++i)
-        {
-            if (FD_ISSET(i, &reads))
-            {
+        if (FD_ISSET(socket_peer, &reads)) {
+            char read[4096];
+            int bytes_received = recv(socket_peer, read, 4096, 0);
+            if (bytes_received < 1) {
+                printf("Connection closed by peer.\n");
+                break;
+            }
+            printf("Received (%d bytes): %.*s",
+                    bytes_received, bytes_received, read);
+        }
 
-                if (i == socket_listen)
-                {
-                    struct sockaddr_storage client_address;
-                    socklen_t client_len = sizeof(client_address);
-                    SOCKET socket_client = accept(socket_listen,
-                                                  (struct sockaddr *)&client_address,
-                                                  &client_len);
-                    if (!ISVALIDSOCKET(socket_client))
-                    {
-                        fprintf(stderr, "accept() failed. (%d)\n",
-                                GETSOCKETERRNO());
-                        return 1;
-                    }
+#if defined(_WIN32)
+        if(_kbhit()) {
+#else
+        if(FD_ISSET(0, &reads)) {
+#endif
+            char read[4096];
+            if (!fgets(read, 4096, stdin)) break;
+            printf("Sending: %s", read);
+            int bytes_sent = send(socket_peer, read, strlen(read), 0);
+            printf("Sent %d bytes.\n", bytes_sent);
+        }
+    } //end while(1)
 
-                    FD_SET(socket_client, &master);
-                    if (socket_client > max_socket)
-                        max_socket = socket_client;
-
-                    char address_buffer[100];
-                    getnameinfo((struct sockaddr *)&client_address,
-                                client_len,
-                                address_buffer, sizeof(address_buffer), 0, 0,
-                                NI_NUMERICHOST);
-                    printf("New connection from %s\n", address_buffer);
-                }
-                else
-                {
-                    char read[1024];
-                    int bytes_received = recv(i, read, 1024, 0);
-                    if (bytes_received < 1)
-                    {
-                        FD_CLR(i, &master);
-                        CLOSESOCKET(i);
-                        continue;
-                    }
-
-                    int j;
-                    for (j = 0; j < bytes_received; ++j)
-                        read[j] = toupper(read[j]);
-                    send(i, read, bytes_received, 0);
-                }
-
-            } // if FD_ISSET
-        }     // for i to max_socket
-    }         // while(1)
-
-    printf("Closing listening socket...\n");
-    CLOSESOCKET(socket_listen);
+    printf("Closing socket...\n");
+    CLOSESOCKET(socket_peer);
 
 #if defined(_WIN32)
     WSACleanup();
 #endif
 
     printf("Finished.\n");
-
     return 0;
 }
+
