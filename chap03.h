98a533b625de0fb51b44eef36e8ca3a72263db84
Emmanuel Peter <Peterojonilemi@gmail.com>
2022-08-03 15:11:03 +0100

    another random push to repo

---
+++ chap03.h
@@ -0,0 +1,7 @@
+#include "network_libraries.h"
+#include <string.h>
+#if defined(_WIN32)
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+#endif
